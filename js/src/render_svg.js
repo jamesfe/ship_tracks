3233989c9b8736d2ae7fea029992446bf99f6244
jamesfe <james.ferrara@gmail.com>
2017-09-06 16:54:54 +0200

    half baked

--- js/src/render_svg.js
+++ js/src/render_svg.js
@@ -1,6 +1,9 @@
 var d3 = require('d3');
 
-
+d3.json('./assets/data/sample.json', function(error, data) {
+  if (error) throw error;
+  features = 
+});
 
 svg.append("path")
     .datum({type: "FeatureCollection", features: features})
