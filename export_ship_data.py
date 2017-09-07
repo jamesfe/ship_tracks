e56f901b8c27a5360190c6a4168b521a69a0fcae
jamesfe <james.ferrara@gmail.com>
2017-09-07 17:26:34 +0200

    Fix minor ship data bug

--- export_ship_data.py
+++ export_ship_data.py
@@ -3,6 +3,8 @@
 import fiona
 import json
 
+print('[\n')
 with fiona.open('./data/EastCoastVesselTracklines2011.gdb/', 'r') as infile:
     for k in infile.items():
-        print(json.dumps(k))
+        print(json.dumps(k[1]) + ', ')
+print('\n]')
