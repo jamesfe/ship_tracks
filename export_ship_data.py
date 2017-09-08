eb919b4e7d1108a56d7a00a05d6ab13fd3a0f1a9
jamesfe <james.ferrara@gmail.com>
2017-09-08 13:16:22 +0200

    Refactor export ship data

--- export_ship_data.py
+++ export_ship_data.py
@@ -3,8 +3,21 @@
 import fiona
 import json
 
-print('[\n')
-with fiona.open('./data/EastCoastVesselTracklines2011.gdb/', 'r') as infile:
-    for k in infile.items():
-        print(json.dumps(k[1]) + ', ')
-print('\n]')
+
+def print_with_delim(delim=''):
+    with fiona.open('./data/EastCoastVesselTracklines2011.gdb/', 'r') as infile:
+        for k in infile.items():
+            print(json.dumps(k[1]) + delim)
+
+
+def export_as_one_json():
+    print('[\n')
+    print_with_delim(',')
+    print(']')
+
+
+def main():
+    print_with_delim()
+
+
+main()
