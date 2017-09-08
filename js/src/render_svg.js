e33d763a437bb872a8aa823571aac6561d6432af
jamesfe <james.ferrara@gmail.com>
2017-09-08 17:14:58 +0200

    web side changes

--- js/src/render_svg.js
+++ js/src/render_svg.js
@@ -32,10 +32,50 @@ var boundingBox = {
       }
     };
 
-
-d3.json('./assets/data/10k_ship_data.json', function(error, data) {
-// d3.json('./assets/data/10_ship_tracks_2011.json', function(error, data) {
+d3.json('./assets/data/countries/countries.geo.json', function(error, data) {
   if (error) throw error;
+  var features = data.features;
+  var svg = d3.select('#ship_plot'),
+    margin = {top: 20, right: 50,bottom: 20, left: 30},
+    width = svg.attr("width") - margin.left - margin.right,
+    height = svg.attr("height") - margin.top - margin.bottom;
+
+  // var projection = d3.geoEquirectangular();
+  // var path = d3.geoPath().projection(projection);
+  // projection.fitExtent([[0,0],[height, width]], boundingBox);
+  // var projection = d3.geoAlbers();
+  var projection = d3.geoEquirectangular();
+  var path = d3.geoPath(projection);
+
+  console.log(projection.clipExtent());
+  // projection.clipExtent(d3.extent(features.map(function(a) { return path.bounds(a)[0]; })));
+  // projection.fitSize([height, width], boundingBox);
+  projection.fitSize([width, height], boundingBox);
+  projection.center([-75, 25]);
+  projection.scale(400);
+  console.log('zoom: ', projection.scale());
+  console.log(projection.clipExtent());
+  console.log(projection.center());
+/*
+  svg.append("path")
+    .datum({type: "FeatureCollection", features: features})
+    .attr("d", path)
+    .attr("class", "blah");
+*/
+  d3.json('./assets/data/daily/283.json', function(error, data) {
+    if (error) throw error;
+    var lines = data.map(function(a) { return a.geometry; });
+    svg.append("path")
+      .datum({type: "FeatureCollection", features: data})
+      .attr("d", path)
+      .attr("class", "lines");
+    console.log("Done");
+  });
+});
+
+/*
+d3.json('./assets/data/daily/283.json', function(error, data) {
+// d3.json('./assets/data/10_ship_tracks_2011.json', function(error, data) {
   var features = data.map(function(a) { return a.geometry; });
 
   //features.forEach(function(a) { debugger; console.log(d3.geo.bounds(a)); });
@@ -44,20 +84,17 @@ d3.json('./assets/data/10k_ship_data.json', function(error, data) {
     margin = {top: 20, right: 50,bottom: 20, left: 30},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom;
-  /*
-  var projection = d3.geoMercator();
-  projection
-    .scale(200)
-    .center([-72, 30]);
-  */
-  // var projection = d3.geoAlbersUsa().scale(1000);
+   // var projection = d3.geoAlbersUsa().scale(1000);
   var projection = d3.geoEquirectangular();
   console.log(projection.scale());
   var path = d3.geoPath().projection(projection);
   // projection.clipExtent = d3.extent(features.map(function(a) { return path.bounds(a)[0]; }));
   projection.fitExtent([[0,0],[height, width]], boundingBox);
-  // projection.fitExtent([[0,0],[height, width]], features[0]);
   console.log(projection.scale());
+  // projection.fitExtent([[0,0],[height, width]], features[0]);
+  var zoom = projection.scale();
+  console.log(projection.scale());
+  projection.scale(zoom/1000);
   console.log(projection.scale());
 
   svg.selectAll("path")
@@ -66,3 +103,5 @@ d3.json('./assets/data/10k_ship_data.json', function(error, data) {
     .attr("d", path)
     .attr("class", "blah");
  });
+
+*/
