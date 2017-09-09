e459ab3a91a34069edc78ca447b144d5085406f6
jamesfe <james.ferrara@gmail.com>
2017-09-09 11:03:12 +0200

    Automate

---
+++ js/get_data.sh
@@ -0,0 +1,4 @@
+mkdir -p ./public/assets/data/countries/
+mkdir -p ./public/assets/data/daily/
+
+wget https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json -O ./public/assets/data/countries/countries.geo.json
