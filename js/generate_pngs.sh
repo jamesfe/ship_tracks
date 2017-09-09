7fb0ba12ec96831455c8a07c7b8c6b2d8333628b
jamesfe <james.ferrara@gmail.com>
2017-09-09 16:21:51 +0200

    Major changes to get hurricanes on the map

--- js/generate_pngs.sh
+++ js/generate_pngs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 for i in `seq 1 365`;
 do
-	echo $i
+	printf $i" "
 	node ./src/node_render.js $i
 done
 
 # Make it
 # convert -delay 10 -loop 0 ./output/*.png animated.gif
+# http://www.imagemagick.org/discourse-server/viewtopic.php?t=28672
