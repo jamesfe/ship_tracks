9fc7c81abbdb7ba8fd5c7058bb30042f9d5b63e5
jamesfe <james.ferrara@gmail.com>
2017-09-10 22:25:20 +0200

    Make things sync, then refactor

--- js/src/node_render.js
+++ js/src/node_render.js
@@ -3,18 +3,13 @@ var fs = require('fs');
 
 var boundingBox = {
       "type": "Feature",
-      "properties": {},
       "geometry": {
         "type": "Polygon",
-        "coordinates": [
-          [
-            [ -104.765625, 11.867350911459308 ],
+        "coordinates": [ [ [ -104.765625, 11.867350911459308 ],
             [ -48.515625, 11.867350911459308 ],
             [ -48.515625, 44.59046718130883 ],
             [ -104.765625, 44.59046718130883 ],
-            [ -104.765625, 11.867350911459308 ]
-          ]
-        ]
+            [ -104.765625, 11.867350911459308 ] ] ]
       }
     };
 
@@ -29,10 +24,10 @@ function pad(n) {
 function addDays(days) {
   var dat = new Date(2013, 0, 0);
   dat.setDate(dat.getDate() + days);
-
   return dat;
 }
 
+/* Parse arguments */
 var generateDay = parseInt(process.argv[2]);
 if (generateDay === undefined) {
   throw Error('no day');
@@ -50,7 +45,7 @@ var styles = {
 }
 
 const D3Node = require('d3-node');
-const d3n = new D3Node(styles);      // initializes D3 with container element
+const d3n = new D3Node(styles);
 var svg = d3n.createSVG(width, height);
 
 svg.append("rect")
@@ -73,76 +68,77 @@ svg.append("text")
   .attr("fill", "black")
   .text(dstring);
 
+/* Draw the country boundaries */
 var countriesFile = './public/assets/data/countries/countries.geo.json';
-// var countriesFile = './public/assets/data/countries/cuba_2.geo.json';
-//var countriesFile = './public/assets/data/countries/usa_4.geo.json';
-//var countriesFile = './public/assets/data/countries/selected.geojson';
 
-fs.readFile(countriesFile, 'utf-8', function(error, data) {
-  if (error) throw error;
-  data = JSON.parse(data);
-  var features = data.features;
+var countriesFileData = fs.readFileSync(countriesFile, 'utf-8');
+var countriesData = JSON.parse(countriesFileData);
+var features = countriesData.features;
 
-  var projection = d3.geoEquirectangular();
-  var path = d3.geoPath(projection);
+var projection = d3.geoEquirectangular();
+var path = d3.geoPath(projection);
 
-  projection.fitSize([width, height], boundingBox);
-  projection.center([-72, 34]);
-  projection.scale(2000);
+projection.fitSize([width, height], boundingBox);
+projection.center([-72, 34]);
+projection.scale(2000);
 
-  if (showCountries === true) {
-    svg.append("path")
-      .datum({type: "FeatureCollection", features: features})
-      .attr("d", path)
-      .attr("class", "blah");
+if (showCountries === true) {
+  svg.append("path")
+    .datum({type: "FeatureCollection", features: features})
+    .attr("d", path)
+    .attr("class", "blah");
+}
+// tgt_day_file = `./public/assets/data/daily/${generateDay}.json`
+tgt_day_file = `../data/2011daily/${generateDay}.json`
+
+/* Read the ship lines file synchronously */
+
+var shipFileData = fs.readFileSync(tgt_day_file, 'utf-8');
+shipData = JSON.parse(shipFileData);
+var lines = shipData.map(function(a) { return a.geometry; });
+if (showShipTracks === true) {
+  svg.append("path")
+    .datum({type: "FeatureCollection", features: shipData})
+    .attr("d", path)
+    .attr("class", "lines");
+}
+
+/* Now we render some hurricanes. */
+var hurricaneFile = './public/assets/data/hurricanes.geojson';
+
+var hdata = fs.readFileSync(hurricaneFile, 'utf-8');
+var hurricaneData = JSON.parse(hdata);
+
+var checkDate = function(obj) {
+  if ((parseInt(obj.properties.day) ===  day) && (parseInt(obj.properties.month)  === month)) {
+    return true;
   }
-  // tgt_day_file = `./public/assets/data/daily/${generateDay}.json`
-  tgt_day_file = `../data/2011daily/${generateDay}.json`
-  fs.readFile(tgt_day_file, 'utf-8', function(error, data) {
-    if (error) throw error;
-    data = JSON.parse(data);
-    var lines = data.map(function(a) { return a.geometry; });
-    if (showShipTracks === true) {
-      svg.append("path")
-        .datum({type: "FeatureCollection", features: data})
-        .attr("d", path)
-        .attr("class", "lines");
-    }
+  return false;
+}
 
-    /* Now we render some hurricanes. */
-    var hurricaneFile = './public/assets/data/hurricanes.geojson';
-    fs.readFile(hurricaneFile, 'utf-8', function(herror, hdata) {
-      if (herror) throw herror;
-      var hurricaneData = JSON.parse(hdata);
+var hfeatures = hurricaneData.filter(checkDate);
 
-      var checkDate = function(obj) {
-        if ((parseInt(obj.properties.day) ===  day) && (parseInt(obj.properties.month)  === month)) {
-          return true;
-        }
-        return false;
-      }
+svg.selectAll("circle4")
+  .data(hfeatures).enter()
+  .append("circle")
+  .attr("cx", function (d) { return projection(d.geometry.coordinates)[0]; })
+  .attr("cy", function (d) { return projection(d.geometry.coordinates)[1]; })
+  .attr("r", function (d) { return (d.properties.windspeed/3) + "px"; })
+  .attr("fill", "#e04314")
 
-      var hfeatures = hurricaneData.filter(checkDate);
 
-      svg.selectAll("circle4")
-        .data(hfeatures).enter()
-        .append("circle")
-        .attr("cx", function (d) { return projection(d.geometry.coordinates)[0]; })
-        .attr("cy", function (d) { return projection(d.geometry.coordinates)[1]; })
-        .attr("r", function (d) { return (d.properties.windspeed/3) + "px"; })
-        .attr("fill", "#e04314")
+/* Draw a box around everything. */
 
-      svg.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", width)
-        .attr("height", height)
-        .attr("class", "bord");
+svg.append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", width)
+  .attr("height", height)
+  .attr("class", "bord");
 
-      const svg2png = require("svg2png");
-        svg2png(d3n.svgString())
-          .then(buffer => fs.writeFileSync(`./output2011/${outputDay}.png`, buffer))
-          .catch(e => console.error(e));
-    });
-  });
-});
+/* Output a SVG in PNG format. */
+const svg2png = require("svg2png");
+  svg2png(d3n.svgString())
+    .then(buffer => fs.writeFileSync(`./output2011/${outputDay}.png`, buffer))
+    .catch(e => console.error(e));
+
