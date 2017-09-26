262770b9db7025d1fdebe738d2c1a840f7d09bb7
jamesfe <james.ferrara@gmail.com>
2017-09-26 09:26:15 +0200

    Fixups, change path

--- js/get_data.sh
+++ js/get_data.sh
@@ -1,4 +1,4 @@
-mkdir -p ./public/assets/data/countries/
-mkdir -p ./public/assets/data/daily/
+mkdir -p ../data/countries/
+mkdir -p ../data/daily/
 
 wget https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json -O ./public/assets/data/countries/countries.geo.json
