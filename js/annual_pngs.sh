e0fc1e78d8a4602cc4ef7b2e6455c67ff8efbc79
jamesfe <james.ferrara@gmail.com>
2017-09-27 22:48:58 +0200

    Do away with ordering

--- js/annual_pngs.sh
+++ js/annual_pngs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-for i in `seq 69 365`;
-do
-	printf $i" "
-	node ./node_render.js $i
-done
+#for i in `seq 1 365`;
+#do
+#	printf $i" "
+#	node ./node_render.js $i
+#done
 
 # Make it
-convert -delay 10 -loop 0 @../output/2013/file.order ../output/2013/*.png $(date +%s).gif
-convert -delay 15 -loop 0 @../output/2013/file.order ../output/2013/*.png $(date +%s).gif
-convert -delay 20 -loop 0 @../output/2013/file.order ../output/2013/*.png $(date +%s).gif
+convert -delay 10 -loop 0 ../output/2013/*.png $(date +%s).gif
+convert -delay 15 -loop 0 ../output/2013/*.png $(date +%s).gif
+convert -delay 20 -loop 0 ../output/2013/*.png $(date +%s).gif
 # http://www.imagemagick.org/discourse-server/viewtopic.php?t=28672
