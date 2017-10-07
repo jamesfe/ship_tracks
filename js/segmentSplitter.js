afe95e142c1c298c8e7c926e74f69824ace43f92
jamesfe <james.ferrara@gmail.com>
2017-10-07 20:51:52 +0200

    Working copy of segment splittergit add segmentSplitter.js!

--- js/segmentSplitter.js
+++ js/segmentSplitter.js
@@ -1,5 +1,10 @@
 /*
  * First off: This is a world of [lat, lon] coordinates.
+ *
+ * What the code in this file does is this:
+ *  - given an input for a file will read the file and for each feature in the file
+ *  returns a map of hours -> features.
+ *  - These features should then be dumped to another file where they can be read by another process.
  * */
 
 const LAT = 0;
@@ -38,17 +43,16 @@ function bucketToHours (inFeature) {
   while (endTime.getTime() >= currentBucket.getTime()) {
     /* We create a map of hours where we will put segments. */
     /* The bucket contains segments which occur in the 60 minutes after the key. */
-    buckets[currentBucket] = []; // an empty array to which we will append coordinates.
     if (count === 0) {
       firstHourSeconds = (startTime.getMinutes() * 60) + startTime.getSeconds();
       const firstDistanceKm = firstHourSeconds * kmPerSecond;
       var sliced = turf.lineSliceAlong(inFeature, 0, firstDistanceKm, 'kilometers');
-      buckets[currentBucket].push(sliced);
+      buckets.set(currentBucket.getTime(), sliced);
     } else {
       var startSliceKm = (firstHourSeconds + (3600 * count)) * kmPerSecond;
       var endSliceKm = (firstHourSeconds + (3600 * count + 1)) * kmPerSecond;
       var sliced = turf.lineSliceAlong(inFeature, startSliceKm, endSliceKm, 'kilometers');
-      buckets[currentBucket].push(sliced);
+      buckets.set(currentBucket.getTime(), sliced);
     }
     currentBucket.setHours(currentBucket.getHours() + 1);
     count++;
@@ -57,17 +61,25 @@ function bucketToHours (inFeature) {
 }
 
 function main (targetFile) {
+  /* Read from an input file and build a map of segments -> hours */
   const inData = require(targetFile);
 
   var hourlyFeatures = new Map();
-  // for (var i = 0; i < inData.length; i++) {
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < inData.length; i++) {
+  //for (var i = 0; i < 10; i++) {
     var feature = inData[i];
     var tempHourly = bucketToHours(feature);
-    console.log(tempHourly);
-    tempHourly.forEach((key, value) => hourlyFeatures[key].append(value));
+    tempHourly.forEach((value, key) => {
+      if (hourlyFeatures.get(key) === undefined) {
+        hourlyFeatures.set(key, [value]);
+      } else {
+        hourlyFeatures.get(key).push(value);
+      }
+    });
   }
-  console.log(tempHourly);
+  hourlyFeatures.forEach((v, k) => {
+    console.log(new Date(k), v.length);
+  });
 }
 
 module.exports = {
